ce0945535bddd6269382992333fe1c687057e55a
joao coura <joaovitorsouzacoura@gmai.com>
2024-02-28 07:09:06 -0300

    feat: registering routes.

--- users_service/public/index.php
+++ users_service/public/index.php
@@ -1,2 +1,31 @@
 <?php
 
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
+$routes = new RouteCollection;
+$routes->addNamePrefix("/api/v1/users");
+
+$userStore = new Route("", [
+    "_controller" => UserController::class,
+    "_action" => "store"
+], methods: ["POST"]);
+
+$userShow = new Route("/{id}", [
+    "_controller" => UserController::class,
+    "_action" => "show"
+], methods: ["GET"]);
+
+$userUpdate = new Route("/{id}", [
+    "_controller" => UserController::class,
+    "_action" => "update"
+], methods: ["PUT"]);
+
+$userDelete = new Route("/{id}", [
+    "_controller" => UserController::class,
+    "_action" => "destroy"
+], methods: ["DELETE"]);
+
+$routes->add("user_list", $userList);
+
+
