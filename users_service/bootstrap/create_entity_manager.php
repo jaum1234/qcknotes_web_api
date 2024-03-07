67f332f5b2a02ff1de1c311cb808a2b8994f0ea2
joao coura <joaovitorsouzacoura@gmai.com>
2024-03-07 07:33:08 -0300

    feat: removing unnecessary bootstrap file for creating entity manager.

--- users_service/bootstrap/create_entity_manager.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Joaocoura\UsersService\Helpers\EntityManagerCreator;
-
-$dbconfig = require_once __DIR__ . "/../config/database.php";
-
-$creator = new EntityManagerCreator();
-$entityManager = $creator->getEntityManager($dbconfig);
